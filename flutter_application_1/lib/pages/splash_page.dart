1700d70ac434a5ba67e9ae350260db6862f2fec5
Melak-tefera <tmeleak@gmail.com>
2026-08-20 16:19:43 +0100

    working with spalsh screen

--- flutter_application_1/lib/pages/splash_page.dart
+++ flutter_application_1/lib/pages/splash_page.dart
@@ -6,7 +6,32 @@ SplashPage({super.key});
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Column(),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Simple Notes",
+              style: TextStyle(
+                color: Colors.lightGreen,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+          
+              ),
+              ),
+              Text("Capture your thoughts instantly",
+              style: TextStyle(
+                color: Colors.lightGreen[150],
+                
+              ),
+              )
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
